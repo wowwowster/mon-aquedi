8de5f1b4f48c5fe6c1c788dd2aa1e0a9198d6bf6
Carl <wowwowster@gmail.com>
2019-01-01 11:58:29 +0100

    Initial commit

---
+++ src/main/java/com/claurier/MonAquedi/MonAquediApplication.java
@@ -0,0 +1,14 @@
+package com.claurier.MonAquedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonAquediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonAquediApplication.class, args);
+	}
+
+}
+
